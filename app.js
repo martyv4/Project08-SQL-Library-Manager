f267c8526af0b91b32a86128a864c0666d0777fc
martyv4 <51241525+martyv4@users.noreply.github.com>
2019-08-20 23:15:09 -0400

    commit

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.get('/', (req, res, next) => {
 app.get('/books', (req, res, next) => {
   try
   {
+      next(new Error('Request could not be fulfilled'));
+
       //https://stackoverflow.com/questions/47800245/node-pagination-with-express
       let page = parseInt(req.query.page);
       if (!page) {  //if page == null
@@ -232,6 +234,23 @@ catch (e) {
   next(new Error('Request could not be fulfilled'));
 }
 });
+
+//default route - respond to anything besides those above
+app.use((req, res, next) => {
+  console.log("Requested route is undefined.");
+  res.render("page-not-found");
+});
+
+const newLocal = 500;
+//error route: reached from any of the non-default in the
+//event of an error (error handler)
+app.use((err, req, res, next) => {
+    console.log(err);    
+    if(!res.headersSent){
+      res.status(newLocal);
+      res.render('error', {error: err});
+    }
+  });
 
 //Host
 const portNumber = 3000;
