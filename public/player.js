e735cf5e8b7b52fa82f214b008737f84855954c2
martyv4 <51241525+martyv4@users.noreply.github.com>
2019-08-23 17:38:53 -0400

    commit updates
    submit

---
+++ public/player.js
@@ -0,0 +1,11 @@
+const SoundCloudAudio = require('soundcloud-audio');
+const player = new SoundCloudAudio('95f22ed54a5c297b1c41f72d713623ef');
+
+window.play = function () {
+    player.resolve("https://soundcloud.com/soundcloud-circuits/sets/downward-facing-drone-taking", function (track) {
+        player.play();
+    });
+}
+
+window.pause = function () {
+    player.pause();
